b1f887e5207e4602ee64a133fd38832b564c39f5
whpark <45619217+whpark@users.noreply.github.com>
2023-07-25 22:59:32 +0900

    .

--- MainWnd.cpp
+++ MainWnd.cpp
@@ -4,7 +4,7 @@
 xMainWnd::xMainWnd(QWidget *parent) : base_t(parent) {
     ui.setupUi(this);
 
-	// CI Bitmap
+	// Application Icon
 	QIcon* icon = new QIcon(":/image/icon.png");
 	setWindowIcon(*icon);
 }
